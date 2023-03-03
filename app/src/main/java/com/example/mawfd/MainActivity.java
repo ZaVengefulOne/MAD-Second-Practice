53d4c51dab771b8682e920e66190169adc875dc5
ZaVengefulOne <shalomeenko53@gmail.com>
2023-03-03 22:29:46 +0300

    Added second activity & also modified some broken stuff

--- app/src/main/java/com/example/mawfd/MainActivity.java
+++ app/src/main/java/com/example/mawfd/MainActivity.java
@@ -2,20 +2,33 @@ package com.example.mawfd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.util.Log;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-private static final String TAG = "AWFD";
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "AWFD";
+    private static final String KEY = "key";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Вывод лога при нажатии");
-            }
-        });
+        Button buttonBack = (Button)findViewById(R.id.button1);
+        buttonBack.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(KEY, "data");
+        Log.i(TAG, "Changed activities!");
+        startActivity(intent);
+    }
+    public void Click2(View view){
+        Log.i(TAG, "Вывод лога при нажатии");
+    }
+    private void parseArgs(){
+        String arg1 = getIntent().getStringExtra(MainActivity.KEY);
+        Log.d(TAG, arg1);
     }
 }
--- app/src/main/java/com/example/mawfd/SecondActivity.java
+++ app/src/main/java/com/example/mawfd/SecondActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class SecondActivivty extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second_activivty);
+        setContentView(R.layout.activity_second);
     }
 }
