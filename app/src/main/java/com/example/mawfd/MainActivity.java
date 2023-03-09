e54077164a890f8df12b1c4ab367b33a2cd34fd4
ZaVengefulOne <shalomeenko53@gmail.com>
2023-03-09 23:32:36 +0300

    Final commit for the second practice

--- app/src/main/java/com/example/mawfd/MainActivity.java
+++ app/src/main/java/com/example/mawfd/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mawfd;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,18 +16,33 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "AWFD";
     private static final String KEY = "key";
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                Log.i(TAG, String.valueOf(result));
+                }
+            });
+
+
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button buttonBack = (Button)findViewById(R.id.button1);
+        Button buttonLog = (Button)findViewById(R.id.button2);
         buttonBack.setOnClickListener(this);
+        buttonLog.setOnClickListener(this::Click2);
     }
     @Override
     public void onClick(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
         intent.putExtra(KEY, "data");
         Log.i(TAG, "Changed activities!");
-        startActivity(intent);
+        mStartForResult.launch(intent);
     }
     public void Click2(View view){
         Log.i(TAG, "Вывод лога при нажатии");
@@ -31,4 +51,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String arg1 = getIntent().getStringExtra(MainActivity.KEY);
         Log.d(TAG, arg1);
     }
+
 }
