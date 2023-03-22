869c93e754412cada0f3fc3d21b9f43649838645
ZaVengefulOne <shalomeenko53@gmail.com>
2023-03-22 21:21:06 +0300

    Created a notification for sixth practice

--- app/src/main/java/com/example/mawfd/StartFrameFragment.java
+++ app/src/main/java/com/example/mawfd/StartFrameFragment.java
@@ -1,15 +1,25 @@
 package com.example.mawfd;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+
 import android.content.Context;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -17,27 +27,60 @@ import androidx.navigation.Navigation;
 import com.example.mawfd.databinding.FragmentStartframeBinding;
 
 
-    public class StartFrameFragment extends Fragment {
-        private FragmentStartframeBinding binding;
-        private static final String TAG = "mafwd";
-        public static final String KEY = "key";
-        public static final String PRIKOL = "example";
-        public StartFrameFragment() {
-            super(R.layout.fragment_startframe);
-        }
+public class StartFrameFragment extends Fragment {
+    private FragmentStartframeBinding binding;
+    private static final String TAG = "mafwd";
+    public static final String KEY = "key";
+    public static final String PRIKOL = "example";
+    private final String CHANNEL_ID = "Channel1";
+    private final int notificationId = 1;
+    private final int requestCode = 2;
 
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            Log.d(TAG, "OnCreateView");
-            Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
-            Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
+    public StartFrameFragment() {
+        super(R.layout.fragment_startframe);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "OnCreateView");
+        Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
+        Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
 //            Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_SHORT).show();
-            binding = FragmentStartframeBinding.inflate(getLayoutInflater());
-            return binding.getRoot();
+        binding = FragmentStartframeBinding.inflate(getLayoutInflater());
+        return binding.getRoot();
+
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_ID, "Channel 1", NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("This is Channel 1");
+            NotificationManager manager = (NotificationManager) getActivity().getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
         }
+    }
+
+    private void showNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(requireContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.awfd_logo_foreground)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText(getString(R.string.notification_text))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(requireContext());
+        if (ActivityCompat.checkSelfPermission(requireContext(), android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.POST_NOTIFICATIONS}, requestCode );
+            return;
+        }
+        notificationManager.notify(
+                notificationId, builder.build()
+        );
+    }
 
         @Override
-        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
+            createNotificationChannel();
             Log.d(TAG, "OnViewCreated");
             Log.d(TAG, "View Lifecycle = " + getViewLifecycleOwner().getLifecycle().getCurrentState().toString());
             Log.d(TAG, "Fragment Lifecycle = " + getLifecycle().getCurrentState().toString());
@@ -66,8 +109,17 @@ import com.example.mawfd.databinding.FragmentStartframeBinding;
                     Navigation.findNavController(view).navigate(R.id.action_startfragment_to_testfragment, bundle);
 
                 }
+
+            });
+            binding.button3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getContext(), "UVEDOMLENIE", Toast.LENGTH_SHORT).show();
+                    showNotification();
+                }
             });
         }
+
 //        public void onClick(View v) {
 //            Bundle result = new Bundle();
 //            result.putString("bundleKey", "result");
